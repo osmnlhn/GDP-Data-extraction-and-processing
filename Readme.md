9324115dfac60579855700818cf75260f2ad4095
osman <osmnlhn@gmail.com>
2024-06-22 21:55:42 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+In this practice project, I will extract data from a website using webscraping and reqeust APIs process it using Pandas and Numpy libraries.
+
+![Alt text](image.png)
+
+Step 1:
+Extract the required GDP data from the given URL using Web Scraping.
+
+Step 2:
+Modify the GDP column of the DataFrame, converting the value available in Million USD to Billion USD.
+
+Step 3:
+Load the DataFrame to the CSV file named "Largest_economies.csv"
+
---
+++ data.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+import pandas as pd
+
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+warnings.filterwarnings('ignore')
+
+URL="https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29"
+
+tables=pd.read_html(URL)
+df=tables[3]
+df.columns = range(df.shape[1])
+df=df[[0,2]]
+df = df.iloc[1:11,:]
+df.columns = ['Country','GDP (Million USD)']
+
+df['GDP (Million USD)'] = df['GDP (Million USD)'].astype(int)
+df[['GDP (Million USD)']] = df[['GDP (Million USD)']]/1000
+df[['GDP (Million USD)']] = np.round(df[['GDP (Million USD)']], 2)
+df.rename(columns = {'GDP (Million USD)' : 'GDP (Billion USD)'})
+
+df.to_csv('./Largest_economies.csv')
